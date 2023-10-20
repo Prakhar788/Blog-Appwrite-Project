fe3c3e2c631e6def32bb761e98d5a67ebf876e04
Prakhar <prakhar6601@gmail.com>
2023-10-20 21:38:51 +0530

    Adding new Files

---
+++ src/appwrite/auth.js
@@ -0,0 +1,56 @@
+//for further ref-https://appwrite.io/docs/products/auth/email-password
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+
+export class AuthService{
+    client=new Client();
+    account;
+    constructor(){
+        this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+        this.account=new Account(this.client);
+    }
+
+    async createAccount({email,password,name}){
+        try {
+        const userAccount=  await this.account.create(ID.unique(),email,password,name);
+        if(userAccount){
+            return this.login({email,password});
+        }
+        else{
+            return userAccount;
+        }
+        } catch (error) {
+            throw error;
+        }
+
+    }
+    async login({email,password}){
+        try {
+           return await this.account.createEmailSession(email,password);
+
+        } catch (error) {
+         throw error;   
+        }
+    }
+    async getCurrentUser(){
+        try {
+            await this.account.get();
+        } catch (error) {
+            console.log("getCurrentUser::error",error)
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("logout::error",error)
+        }
+    }
+
+}
+const authService=new AuthService();
+
+export default authService; 
+
